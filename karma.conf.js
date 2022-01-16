da60c8cf48f5fa53a85d1a898d7ab4cb0a93c934
Tiago Siqueira <tvsjazz@gmail.com>
2022-01-16 18:17:44 -0300

    config karma

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,9 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
